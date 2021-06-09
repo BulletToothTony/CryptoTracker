931e6b6c6b1e07b5cafc831ac901a0a8b498d9cc
Henry WL <henrywl@gmail.com>
2021-06-09 15:28:56 +0100

    XMR DOGE

--- script.js
+++ script.js
@@ -29,6 +29,21 @@ low24ADA = document.getElementById('low24ADA');
 price24ADA = document.getElementById('price24ADA');
 percent24ADA = document.getElementById('percent24ADA')
 
+imgsrcXMR = document.getElementById('imgsrcXMR');
+priceXMR = document.getElementById('priceXMR');
+high24XMR = document.getElementById('high24XMR');
+low24XMR = document.getElementById('low24XMR');
+price24XMR = document.getElementById('price24XMR');
+percent24XMR = document.getElementById('percent24XMR')
+
+imgsrcDOGE = document.getElementById('imgsrcDOGE');
+priceDOGE = document.getElementById('priceDOGE');
+high24DOGE = document.getElementById('high24DOGE');
+low24DOGE = document.getElementById('low24DOGE');
+price24DOGE = document.getElementById('price24DOGE');
+percent24DOGE = document.getElementById('percent24DOGE')
+
+
 async function btc(){
     let response = await fetch("https://api.coingecko.com/api/v3/coins/markets?ids=bitcoin&vs_currency=usd")
     let json = await response.json()
@@ -81,7 +96,36 @@ async function ada() {
     imgsrcADA.src = json[0].image
 }
 
+async function xmr() {
+    let response = await fetch("https://api.coingecko.com/api/v3/coins/markets?ids=monero&vs_currency=usd")
+    let json = await response.json()
+    console.log(json[0])
+
+    priceXMR.textContent = json[0].current_price
+    high24XMR.textContent = json[0].high_24h
+    low24XMR.textContent = json[0].low_24h
+    price24XMR.textContent = json[0].price_change_24h
+    percent24XMR.textContent = json[0].price_change_percentage_24h
+    imgsrcXMR.src = json[0].image
+
+}
+
+async function doge() {
+    let response = await fetch("https://api.coingecko.com/api/v3/coins/markets?ids=dogecoin&vs_currency=usd")
+    let json = await response.json()
+    console.log(json[0])
+
+    priceDOGE.textContent = json[0].current_price
+    high24DOGE.textContent = json[0].high_24h
+    low24DOGE.textContent = json[0].low_24h
+    price24DOGE.textContent = json[0].price_change_24h
+    percent24DOGE.textContent = json[0].price_change_percentage_24h
+    imgsrcDOGE.src = json[0].image
+}
+
 btc()
 eth()
 ltc()
 ada()
+xmr()
+doge()
